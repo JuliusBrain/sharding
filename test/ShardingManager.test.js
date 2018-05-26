41587bf7b1fec21e3a628c76e8af9d375262d5d5
Adrian Hetman <adrian.hetman@espeo.eu>
2018-05-26 15:55:41 +0200

    restructure test code, added test utils file

--- test/ShardingManager.test.js
+++ test/ShardingManager.test.js
@@ -1,43 +1,8 @@
 /* global artifacts, contract, it, beforeEach, web3 */
 /* eslint no-unused-expressions: 0 */
 
-
+const { mineBlock, mineNBlocks, addSeconds, EVMError, ether} = require('./utils/testUtils.js');
 const ShardingManager = artifacts.require('ShardingManager');
-
-require('chai')
-  .use(require('chai-as-promised'))
-  .use(require('chai-bignumber')())
-  .should();
-
-
-const mineBlock = () => (
-new Promise((resolve, reject) =>
-    web3.currentProvider.sendAsync({
-    jsonrpc: '2.0',
-    method: 'evm_mine',
-    id: new Date().getTime(),
-    }, (error, result) => (error ? reject(error) : resolve(result.result))))
-);
-
-const mineNBlocks = async n => {
-for (let i = 0; i < n; i++) {
-    await mineBlock();
-}
-}
-
-const addSeconds = seconds => (
-new Promise((resolve, reject) =>
-    web3.currentProvider.sendAsync({
-    jsonrpc: '2.0',
-    method: 'evm_increaseTime',
-    params: [seconds],
-    id: new Date().getTime(),
-    }, (error, result) => (error ? reject(error) : resolve(result.result))))
-    .then(mineBlock)
-);
-
-const EVMError = message => `VM Exception while processing transaction: ${message}`;
-const ether = n => new web3.BigNumber(web3.toWei(n, 'ether'))
 
 contract('ShardingManager', (accounts) => {
 
